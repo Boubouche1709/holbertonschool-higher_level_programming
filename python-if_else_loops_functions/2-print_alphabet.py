c33f18f34cd79214123ce00fbc18550d878b5abc
Boubouche1709 <hadjarflorian@gmail.com>
2025-09-03 08:56:13 +0200

    fix the code

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-for alpha in range(97, 123):
-    print(chr(alpha), end="")
+print("{}".format("".join(chr(letter) for letter in range(97, 123))), end="")
--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for alpha in range(97, 123):
-    if alpha != 101 and alpha != 113:
-        print(chr(alpha), end="")
+print("{}".format("".join(
+    chr(letter) for letter in range(97, 123) if letter != 101 and letter != 113
+)), end="")
