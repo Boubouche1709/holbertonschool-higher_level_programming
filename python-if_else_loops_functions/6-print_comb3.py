5783966232238c72ea790b77b4bb9a986fbcac19
Boubouche1709 <hadjarflorian@gmail.com>
2025-09-03 09:17:47 +0200

    fix the code

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-
-# # Boucle sur le premier chiffre (de 0 à 9)
 for i in range(10):
-    # Boucle sur le deuxième chiffre (toujours plus grand que le premier)
     for j in range(i + 1, 10):
-        # Si ce n'est pas la dernière combinaison, on ajoute ", "
         if i != 8 or j != 9:
-            print(f"{i}{j}", end=", ")
-# Dernière combinaison sans virgule
+            print("{}{}, ".format(i, j), end="")
 print("89")
