ee2717a96651adae1cc2f037e69a04e411640c3f
Boubouche1709 <hadjarflorian@gmail.com>
2025-09-04 08:59:39 +0200

    Add 2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+
+def main():
+    argv = sys.argv[1:]
+    count = len(argv)
+    if count == 0:
+        print("Number of argument(s).")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print(f"{count} arguments:")
+    for i in range(count):
+        print(f"{i + 1}: {argv[i]}")
+
+
+if __name__ == "__main__":
+    main()
