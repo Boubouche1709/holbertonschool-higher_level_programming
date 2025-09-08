e8fe9ed2113e045fd8f6d1cb6c3a5d398f29f8e2
Boubouche1709 <hadjarflorian@gmail.com>
2025-09-08 10:39:21 +0200

    fix the condition for the space at the end of the line

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in row:
-            print("{:d}".format(i), end=" ")
-        print()
+	    if i < len(row) - 1:
+                print("{:d}".format(i), end=" ")
+            else:
+		print("{:d}".format(row[i]))
