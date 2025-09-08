67769e3954d030458344d51013dfe3ca7d7eabb1
Boubouche1709 <hadjarflorian@gmail.com>
2025-09-08 11:04:25 +0200

    Fix the code

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,9 +2,8 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i in range(len(row)):
-            if i < len(row) - 1:
-                print("{:d}".format(row[i]), end=" ")
-            else:
-                print("{:d}".format(row[i]))
-    print()
+        for index in range(len(row)):
+            print("{:d}".format(row[index]), end="")
+            if index < len(row) - 1:
+                print(" ", end="")
+        print()
