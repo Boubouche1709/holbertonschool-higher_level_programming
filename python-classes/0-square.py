4621c1fadc2ccaf1e5d439ea9035469cb0013c5b
Boubouche1709 <hadjarflorian@gmail.com>
2025-09-15 09:19:20 +0200

    Add 0-square.py

---
+++ python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+class Square:
+    """Square is an empty class that defines a square structure.
+    This class currently does not implement any attributes or methods.
+    It serves as a placeholder or base for future square-related logic.
+    """
+    pass
