7f80e8e46179d632316a0f5ebea0229325d57ff2
Boubouche1709 <hadjarflorian@gmail.com>
2025-09-16 06:13:59 +0200

    Add 2-rectangle.py

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Module 0-rectangle
+Defines an empty Rectangle class.
+"""
+
+
+class Rectangle:
+    """Rectangle is an empty class that defines a rectangle structure.
+    This class currently does not implement any attributes or methods.
+    It serves as a placeholder or base for future rectangle-related logic.
+    """
+    @property
+    def width(self)
+    return self.__width
+
+    @property
+    def height(self)
+    return sel.__height
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value)
+    if not isinstance(value int):
+        raise TypeError("height must be an integer")
+    if value < 0:
+        raise ValueError("height must be >= 0")
+    self.__height = height
+
+    def area(self)
+        return self.__width * self.__height
+
+    def perimeter(self)
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (2 * (self.__height + self.__width))
+
