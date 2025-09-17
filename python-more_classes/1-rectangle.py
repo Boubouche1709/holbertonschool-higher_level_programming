b80b18e573cd2797df03e75f86a8952b4c47fd71
Boubouche1709 <hadjarflorian@gmail.com>
2025-09-17 11:33:48 +0200

    fix the synthaxe error

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -13,11 +13,11 @@ class Rectangle:
         self.height = height
 
     @property
-    def width(self)
+    def width(self):
     return self.__width
 
     @property
-    def height(self)
+    def height(self):
     return self.__height
 
     @width.setter
@@ -30,7 +30,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value)
-    if not isinstance(value int):
+    if not isinstance(value, int):
         raise TypeError("height must be an integer")
     if value < 0:
         raise ValueError("height must be >= 0")
