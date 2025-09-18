09d990e3e119c8d32700cde3e35e548a747682f7
Boubouche1709 <hadjarflorian@gmail.com>
2025-09-18 14:47:42 +0200

    Add 6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module defines an empty class"""
+
+
+class BaseGeometry:
+    """Define a class BaseGeometry"""
+    def area(self):
+        """Raises an exception because area is not implemented."""
+        raise Exception("area() is not implemented")
