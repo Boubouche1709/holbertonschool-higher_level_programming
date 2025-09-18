bb45e179cc9606e3892e971ad229cfb6fe343460
Boubouche1709 <hadjarflorian@gmail.com>
2025-09-18 11:31:15 +0200

    Add 1-my_list.py

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a subclass of list a method to print sorted elements."""
+
+
+class MyList(list):
+    """MyList is a subclass of that adds a method to print the list sorted."""
+
+    def print_sorted(self):
+        """Print the list elements in ascending sorted order."""
+        print(sorted(self))
