5662181554525a02bc23fa3f11c9ae93ab79517e
Boubouche1709 <hadjarflorian@gmail.com>
2025-09-18 08:55:58 +0200

    fix the pycodestyle

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""Module 0-rectangle
-Defines an empty Rectangle class.
-"""
+"""Defines a Rectangle class."""
 
 
 class Rectangle:
@@ -9,8 +7,7 @@ class Rectangle:
     This class currently does not implement any attributes or methods.
     It serves as a placeholder or base for future rectangle-related logic.
     """
-
-    number_of_instance = 0
+    number_of_instances = 0
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
@@ -19,12 +16,12 @@ class Rectangle:
         Rectangle.number_of_instances += 1
 
     @property
-    def width(self)
-    return self.__width
+    def width(self):
+        return self.__width
 
     @property
-    def height(self)
-    return sel.__height
+    def height(self):
+        return self.__height
 
     @width.setter
     def width(self, value):
@@ -35,20 +32,20 @@ class Rectangle:
         self.__width = value
 
     @height.setter
-    def height(self, value)
-    if not isinstance(value int):
-        raise TypeError("height must be an integer")
-    if value < 0:
-        raise ValueError("height must be >= 0")
-    self.__height = height
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
 
-    def area(self)
-    return self.__width * self.__height
+    def area(self):
+        return (self.height * self.__width)
 
-    def perimeter(self)
-    if self.__width == 0 or self.__height == 0:
-        return 0
-    return (2 * (self.__height + self.__width))
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (2 * (self.__height + self.width))
 
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
@@ -58,7 +55,8 @@ class Rectangle:
         return "\n".join(rows)
 
     def __repr__(self):
-        return "Rectangle({}, {})".format(self.__width, self.__height)
+        return "Rectangle({}, {})".format(self.__width, self.height)
 
     def __del__(self):
         print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
