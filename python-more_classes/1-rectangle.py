019e75f3bce02ffc4552f8ef10e440dc2e987a46
Boubouche1709 <hadjarflorian@gmail.com>
2025-09-18 08:31:24 +0200

    fix the pycodestle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -28,7 +28,7 @@ class Rectangle:
         self.__width = value
 
     @height.setter
-    def height(self, value)
+    def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
