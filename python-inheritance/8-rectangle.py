20fb2f007348e8f07b498bb0a3ad0163769679ed
Boubouche1709 <hadjarflorian@gmail.com>
2025-09-19 09:14:40 +0200

    fix the code for requierements

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,19 +2,7 @@
 """This module defines an empty class"""
 
 
-class BaseGeometry:
-    """Define a class BaseGeometry"""
-    def area(self):
-        """Raises an exception because area is not implemented."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validates that value is a positive integer."""
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """Rectangle class inheriting from BaseGeometry"""
