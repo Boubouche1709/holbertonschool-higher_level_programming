23d21ae299d0a6335669227e04da8a6c29d6f4d9
Boubouche1709 <hadjarflorian@gmail.com>
2025-09-19 09:28:56 +0200

    fix the [rectangle] return

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -20,4 +20,4 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         """Return the string print"""
-        return f"[Rectangle {self.__width}/{self.__height}]"
+        return f"[Rectangle] {self.__width}/{self.__height}"
