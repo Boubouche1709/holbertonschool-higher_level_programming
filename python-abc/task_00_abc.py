dcb03f13bceed1c81fc84ec734350e6120364df8
Boubouche1709 <hadjarflorian@gmail.com>
2025-09-24 08:19:15 +0200

    fix the pycodestyle

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
 
-
+# Import ABC (Abstract Base Class) and abstractmethod from the abc module
 from abc import ABC, abstractmethod
 
+
+# Define the abstract base class Animal
 class Animal(ABC):
+    # Declare an abstract method named sound
     @abstractmethod
     def sound(self):
         pass
 
+
+# Define the Dog class that inherits from Animal
 class Dog(Animal):
+
+    # Implement the sound method for Dog
     def sound(self):
         return "Bark"
 
+
+# Define the Cat class that inherits from Animal
 class Cat(Animal):
+    # Implement the sound method for Cat
     def sound(self):
         return "Meow"
-
