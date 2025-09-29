df31ff774bc48c0fdb629a46d8a7ec84cd246e06
Boubouche1709 <hadjarflorian@gmail.com>
2025-09-29 13:18:03 +0200

    Add 9-student.py

---
+++ python-input_output/9-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module defines a Student class with basic attributes
+and a method to retrieve a JSON-serializable dictionary representation.
+"""
+
+
+class Student:
+    """
+    Represents a student with a first name, last name, and age.
+    """
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+
+def to_json(obj):
+    """Returns the dictionary representation of the Student instance"""
+    return self.__dict__
