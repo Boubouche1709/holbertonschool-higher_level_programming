8259acdaf238bc769bb3d08e24af6a19d049c717
Boubouche1709 <hadjarflorian@gmail.com>
2025-09-29 13:20:25 +0200

    Fix the indentation

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -9,13 +9,13 @@ class Student:
     """
     Represents a student with a first name, last name, and age.
     """
-    def __init__(self, first_name, last_name, age):
+    def __init__(self, first_name, last_name , age):
         """Initializes a new Student instance"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
 
-def to_json(self):
+    def to_json(obj):
     """Returns the dictionary representation of the Student instance"""
-    return self.__dict__
+        return self.__dict__
