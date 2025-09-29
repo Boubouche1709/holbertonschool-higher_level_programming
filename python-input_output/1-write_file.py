38a8f7aa6c427b9f62451632eb910509c9843408
Boubouche1709 <hadjarflorian@gmail.com>
2025-09-29 10:16:55 +0200

    fix the filename

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -7,5 +7,5 @@ and returns the number of characters written.
 
 def write_file(filename="", text=""):
     """Write a text and returns the number of characters written."""
-    with open("my_first_file.txt", "w", encoding='utf-8') as f:
+    with open(filename, "w", encoding='utf-8') as f:
         return f.write(text)
