3b90a6440fa3393d207105a60e7b1d43891e0b0f
Boubouche1709 <hadjarflorian@gmail.com>
2025-09-29 13:25:39 +0200

    fix pycodestyle

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -9,12 +9,11 @@ class Student:
     """
     Represents a student with a first name, last name, and age.
     """
-    def __init__(self, first_name, last_name , age):
+    def __init__(self, first_name, last_name, age):
         """Initializes a new Student instance"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
 
     def to_json(self):
         """Returns the dictionary representation of the Student instance"""
