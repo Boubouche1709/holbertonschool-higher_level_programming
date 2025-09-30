0cea9a0360e90dd59bd9899927839e4cc66a7b17
Boubouche1709 <hadjarflorian@gmail.com>
2025-09-30 09:23:00 +0200

    Add 10-student.py

---
+++ python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This module defines a Student class with basic attributes
+and a method to retrieve a JSON-serializable dictionary representation.
+"""
+
+
+class Student:
+    """
+    Represents a student with a first name, last name, and age.
+    """
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieve a dictionary representation of a Student instance.
+        If attrs is a list of strings,
+        only attributes contained in this list must be retrieved.
+        """
+        if isinstance(attrs, list):
+            new_dict = {}
+            for key in attrs:
+                if key in self.__dict__:
+                    new_dict[key] = self.__dict__[key]
+            return new_dict
+        else:
+            return self.__dict__
