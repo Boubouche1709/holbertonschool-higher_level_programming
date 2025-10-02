5dc65eaf96631b793489e15db3fea15835590942
Boubouche1709 <hadjarflorian@gmail.com>
2025-10-02 08:34:54 +0200

    Add task_01_pickle.py

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+
+import pickle
+
+
+class CustomObject:
+    """
+    A class to represent a custom object with serialization capabilities
+
+    Attributes:
+        name (str): The name of the person.
+        age (int): The age of the person.
+        is_student (bool): Whether the person is a student.
+    """
+    def __init__(self, name, age, is_student):
+        """
+        Initializes a new CustomObject instance.
+
+        Args:
+            name (str): The name of the person.
+            age (int): The age of the person.
+            is_student (bool): Student status.
+        """
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        """
+        Displays the object's attributes in a readable format.
+        """
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is student: {self.is_student}")
+
+    def serialize(self, filename):
+        """
+        Serializes the current instance and saves it to a file.
+
+        Args:
+            filename (str): The name of the file where the object will be saved
+
+        Returns:
+            None if an error occurs during serialization.
+        """
+        try:
+            with open(filename, "wb") as f:
+                pickle.dump(self, f)
+        except Exception:
+            return None
+
+    @classmethod
+    def deserialize(cls, filename):
+        """
+        Loads an instance of CustomObject from a pickle file.
+
+        Args:
+            filename (str): The name of the file to load from.
+
+        Returns:
+            CustomObject: A new instance loaded from the file.
+            None: If loading fails due to an exception.
+        """
+        try:
+            with open(filename, "rb") as f:
+                return pickle.load(f)
+        except Exception:
+            return None
