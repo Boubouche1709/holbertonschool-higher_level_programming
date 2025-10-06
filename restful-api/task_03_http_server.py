c0a7dbc5bf0bcf50402d79c84932bad3a4d70b07
Boubouche1709 <hadjarflorian@gmail.com>
2025-10-06 15:35:09 +0200

    delete /info

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -24,16 +24,6 @@ class simplehandler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(json.dumps(data).encode("utf-8"))
 
-        elif self.path == "/info":
-            info = {
-                "version": "1.0",
-                "description": "A simple API built with http.server"
-            }
-            self.send_response(200)
-            self.send_header("Content-type", "application/json")
-            self.end_headers()
-            self.wfile.write(json.dumps(info).encode("utf-8"))
-
         elif self.path == "/status":
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
