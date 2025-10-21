a13eeba5a7251789604e91c0b3fe66168f71452c
Boubouche <hadjarflorian@gmail.com>
2025-10-21 08:39:01 +0200

    Delete SQL_introduction/16-no_link.sql

--- SQL_introduction/16-no_link.sql
+++
@@ -1,7 +0,0 @@
--- Lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
--- Only rows where the name column contains a value.
--- Results display score and name, sorted by descending score.
-SELECT score, name
-FROM second_table
-WHERE name IS NOT NULL AND name != ''
-ORDER BY score DESC;
