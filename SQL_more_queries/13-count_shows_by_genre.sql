15f96a4de6cf5bc7d8d98e26acb8e78c672f2c31
Boubouche1709 <hadjarflorian@gmail.com>
2025-10-21 14:13:31 +0200

    fix the code for tv genre

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,6 +3,6 @@
 SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_genres
 JOIN tv_show_genres
-ON tv_show_genres.genre_id = tv_genres.genre_id
+ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
