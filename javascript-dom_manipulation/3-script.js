ff5f70004fbc0b7989a482a16ace72b04feff5ca
Boubouche1709 <hadjarflorian@gmail.com>
2025-11-10 10:37:42 +0100

    fix the const header

--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -1,5 +1,5 @@
 document.querySelector = ('#toggle_header').addEventListener('click', function () {
-const header = document.querySelector = ('header');
+const header = document.querySelector('header');
 
     if (header.classList.contains('red')) {
         header.classList.replace('red', 'green');
