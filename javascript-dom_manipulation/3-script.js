43e518845c51466632936da07cc3b0a6fd5f6b0b
Boubouche1709 <hadjarflorian@gmail.com>
2025-11-10 10:34:03 +0100

    Add file that toggles the class of the header element when the user clicks on the tag id toggle_header

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,9 @@
+document.querySelector = ('#toggle_header').addEventListener('click', function () {
+const headerElement = document.querySelector = ('header');
+
+    if (headerElement.classList.contains('red')) {
+        headerElement.replace('red', 'green');
+    } else {
+        headerElementclassList.replace('green', 'red');
+    }
+});
