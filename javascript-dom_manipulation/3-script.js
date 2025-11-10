c2fbf5de24dd8fa9cd02f6f5b3cb79cd999ad27b
Boubouche1709 <hadjarflorian@gmail.com>
2025-11-10 10:36:15 +0100

    fix the synthax

--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -2,7 +2,7 @@ document.querySelector = ('#toggle_header').addEventListener('click', function (
 const header = document.querySelector = ('header');
 
     if (header.classList.contains('red')) {
-        header.replace('red', 'green');
+        header.classList.replace('red', 'green');
     } else {
         header.classList.replace('green', 'red');
     }
