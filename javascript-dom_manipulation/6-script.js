7df704554d7b4b36a13182253a5726bdc10784dd
Boubouche1709 <hadjarflorian@gmail.com>
2025-11-10 13:31:09 +0100

    fix the semistandard

--- javascript-dom_manipulation/6-script.js
+++ javascript-dom_manipulation/6-script.js
@@ -3,8 +3,4 @@ fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
   .then(response => response.json())
   .then(data => {
     characterElement.textContent = data.name;
-  })
-  .catch(error => {
-    console.error('Error fetching character:', error);
-    characterElement.textContent = 'Failed to load character';
   });
