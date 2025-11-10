618ff5c73a2e4e9228329cefd82a12326a6ade56
Boubouche1709 <hadjarflorian@gmail.com>
2025-11-10 14:03:20 +0100

    Add file  that fetches from https://hellosalut.stefanbohacek.com/?lang=fr and displays the value of hello from that fetch in the HTML element with id hello

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+  fetch('https://hellosalut.stefanbohacek.com/?lang=fr')
+    .then(response => response.json())
+    .then(data => {
+      const helloElem = document.querySelector('#hello');
+      helloElem.textContent = data.hello;
+  })
+});
